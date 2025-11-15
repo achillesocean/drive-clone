91543b7f91e6e35f4e0fc766c28d27cd85988b11
Felmeta Dereje <0202sent@gmail.com>
2025-11-15 23:39:51 +0300

    database setup

--- src/env.js
+++ src/env.js
@@ -11,6 +11,12 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+
+    SINGLESTORE_PASS: z.string(),
+    SINGLESTORE_USER: z.string(),
+    SINGLESTORE_DB_NAME: z.string(),
+    SINGLESTORE_CONNECTION_STRING: z.string(),
+    SINGLESTORE_PORT: z.string(),
   },
 
   /**
@@ -30,6 +36,12 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+
+    SINGLESTORE_PASS: process.env.SINGLESTORE_PASS,
+    SINGLESTORE_USER: process.env.SINGLESTORE_USER,
+    SINGLESTORE_DB_NAME: process.env.SINGLESTORE_DB_NAME,
+    SINGLESTORE_CONNECTION_STRING: process.env.SINGLESTORE_HOST,
+    SINGLESTORE_PORT: process.env.SINGLESTORE_PORT,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,6 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+// import { createClient, type Client } from "@libsql/client";
+import { drizzle } from "drizzle-orm/singlestore";
+import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -8,12 +9,32 @@ import * as schema from "./schema";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
+
+// explain this caching strategy
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  conn: Pool | undefined;
 };
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+const conn =
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_CONNECTION_STRING,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
 
-export const db = drizzle(client, { schema });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+conn.addListener("error", (err) => {
+  console.error("Database connection error: ", err);
+});
+
+// export const client =
+//   globalForDb.client ?? createClient({ url: env.DATABASE_URL });
+// if (env.NODE_ENV !== "production") globalForDb.client = client;
+
+export const db = drizzle(conn, { schema });
