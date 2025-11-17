dc3d0c73d6fb525dfbdc6e42a0e10b6a8728bf4e
Felmeta Dereje <0202sent@gmail.com>
2025-11-17 14:52:48 +0300

    db models with server action

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,31 @@
+import { mockFiles, mockFolders } from "~/lib/mock-data";
+import { db } from "~/server/db";
+import { files, folders } from "~/server/db/schema";
+
+export default function SandboxPage() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+
+        await db.insert(folders).values(
+          mockFolders.map((folder, index) => ({
+            id: index + 1,
+            name: folder.name,
+            parent: index !== 0 ? 1 : null,
+          }))
+        );
+
+        await db.insert(files).values(
+          mockFiles.map((file, index) => ({
+            id: index + 1,
+            name: file.name,
+            size: 5000,
+            url: file.url,
+            parent: index,
+          }))
+        );
+      }}
+    ></form>
+  );
+}
