8600897b18902ca621b82b4911ec1464fb37e3c6
Felmeta Dereje <0202sent@gmail.com>
2025-11-18 16:06:24 +0300

    schemas renamed

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,9 +1,9 @@
 import type { Folder, File } from "~/lib/mock-data";
 import { Folder as FolderIcon, FileIcon } from "lucide-react";
-import type { files, folders } from "~/server/db/schema";
+import type { files_table, folders_table } from "~/server/db/schema";
 import Link from "next/link";
 
-export function FileRow(props: { file: typeof files.$inferSelect }) {
+export function FileRow(props: { file: typeof files_table.$inferSelect }) {
   const { file } = props;
   return (
     <li
@@ -28,7 +28,9 @@ export function FileRow(props: { file: typeof files.$inferSelect }) {
   );
 }
 
-export function FolderRow(props: { folder: typeof folders.$inferSelect }) {
+export function FolderRow(props: {
+  folder: typeof folders_table.$inferSelect;
+}) {
   const { folder } = props;
   return (
     <li
--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,6 +1,6 @@
 import { mockFiles, mockFolders } from "~/lib/mock-data";
 import { db } from "~/server/db";
-import { files, folders } from "~/server/db/schema";
+import { files_table, folders_table } from "~/server/db/schema";
 
 export default function SandboxPage() {
   return (
@@ -9,7 +9,7 @@ export default function SandboxPage() {
       action={async () => {
         "use server";
 
-        await db.insert(folders).values(
+        await db.insert(folders_table).values(
           mockFolders.map((folder, index) => ({
             id: index + 1,
             name: folder.name,
@@ -17,7 +17,7 @@ export default function SandboxPage() {
           }))
         );
 
-        await db.insert(files).values(
+        await db.insert(files_table).values(
           mockFiles.map((file, index) => ({
             id: index + 1,
             name: file.name,
